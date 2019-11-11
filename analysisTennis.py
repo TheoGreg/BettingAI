b3d2d242f095751c8a60a35ca4563f9453da2cc3
Theophane Gregoir <44373373+TheoGreg@users.noreply.github.com>
2019-11-11 22:32:19 +0000

    Add files via upload
    Adding benefits calculation and outsider prediction analysis

--- analysisTennis.py
+++ analysisTennis.py
@@ -6,7 +6,6 @@ Created on Sun Nov 10 15:15:01 2019
 @author: theophanegregoir
 """
 
-
 import pandas as pd
 import sklearn as sk
 import datetime as dt
@@ -18,7 +17,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.neural_network import MLPClassifier
 
 ### Creation of train and test datasets
 xls_file = pd.ExcelFile('/Users/theophanegregoir/Desktop/SportsCoding/Tennis Data/2002.xls')
@@ -44,7 +43,7 @@ for i in range(2003,2020):
 full['Date'] = ((pd.to_datetime(full['Date']) - temp)/dt.timedelta(days=1)) - 37256
 
 
-###Anonymiser Winners and Losers
+###Anonymiser for Winners and Losers
 s = pd.Series([k for k in range(46018)])
 full.set_index(s)
 full['Winner_AB'] = 0
@@ -73,31 +72,74 @@ new_features_selection = ['Date','Tournament','Round','Best of','Surface','Court
 new_labels_selection = ['Winner_AB']
 A = full[new_features_selection]
 B = full[new_labels_selection]
+A['RankA'] = pd.to_numeric(A['RankA'])
+A['RankB'] = pd.to_numeric(A['RankB'])
 A = pd.get_dummies(A, prefix_sep='_', drop_first = True)
 
-features_train, features_test, labels_train, labels_test = train_test_split(A, B, test_size=0.40, random_state=40)
+features_train, features_test, labels_train, labels_test = train_test_split(A, B, test_size=0.25, random_state=50)
 
 ### MACHINE LEARNING
 print('Training starting...')
 
+
+### ADABOOST CLASSIFIER
 #t0 = time()
-#parameters = {'n_estimators':[i for i in range(1,51,10)], 'learning_rate':[(1.0/j) for j in range(1,6)]}
+#parameters = {'n_estimators':[10,20,30], 'learning_rate':[0.2,0.4,0.8]}
 #ada = AdaBoostClassifier()
 #clf = GridSearchCV(ada, parameters, cv=5)
 #clf.fit(features_train,labels_train)
 #t1 = time()
-#print("Training time : " + str(t1 - t0) + " seconds")
+#print("Training time Adaboost : " + str(t1 - t0) + " seconds")
 #pred = clf.predict(features_test)
 #pred = pd.DataFrame(pred)
 #print(accuracy_score(labels_test, pred))
 #matrix = confusion_matrix(labels_test, pred)
 #print(matrix)
 
-clf2 = RandomForestClassifier(n_estimators = 20,min_samples_split = 5, max_depth = 20)
-clf2.fit(features_train,labels_train)
-pred2 = clf2.predict(features_test)
-print(accuracy_score(labels_test, pred2))
-matrix2 = confusion_matrix(labels_test, pred2)
+### RANDOM FOREST CLASSIFIER
+#t2 = time()
+#clf2 = RandomForestClassifier(n_estimators = 20,min_samples_split = 5, max_depth = 20)
+#clf2.fit(features_train,labels_train)
+#pred2 = clf2.predict(features_test)
+#print(accuracy_score(labels_test, pred2))
+#t3 = time()
+#print("Training time Random Forest : " + str(t3 - t2) + " seconds")
+#matrix2 = confusion_matrix(labels_test, pred2)
+#print(matrix2)
+
+### NEURAL NETWORK
+t4 = time()
+neural = MLPClassifier(activation='relu', tol=1e-8, alpha=1e-05, hidden_layer_sizes=(100,100,100),learning_rate='constant', max_iter = 5000)
+neural.fit(features_train,labels_train)
+predNeural = neural.predict(features_test)
+print(accuracy_score(labels_test, predNeural))
+t5 = time()
+print("Training time Neural Network : " + str(t5 - t4) + " seconds")
+matrix2 = confusion_matrix(labels_test, predNeural)
 print(matrix2)
+
+
+### RESULTS ANALYSIS
+final_table = features_test[['B365A','B365B','RankA','RankB']]
+final_table['Results'] = labels_test
+final_table['Predictions'] = predNeural
+right = (final_table['Predictions'] == final_table['Results'])
+predicted_A = final_table['Predictions'] == 'A'
+predicted_B = final_table['Predictions'] == 'B'
+final_table['earning'] = -1
+final_table.loc[predicted_A & right, 'earning'] = final_table.loc[predicted_A & right,'B365A'] - 1
+final_table.loc[predicted_B & right, 'earning'] = final_table.loc[predicted_B & right,'B365B'] - 1
+print("Betting 1$ on each game and following the AI, your return would have been : " + str(final_table['earning'].sum()) + " $")
+
+
+### OUTSIDERS PREDICTED
+bool_1 = final_table['Predictions'] == 'A'
+bool_2 = final_table['B365A'] > final_table['B365B']
+bool_3 = final_table['Predictions'] == 'B'
+bool_4 = final_table['B365B'] > final_table['B365A']
+Out_pred = final_table[(bool_1 & bool_2)  | (bool_3 & bool_4)]
+right_out = (Out_pred['Predictions'] == Out_pred['Results'])
+print("When betting on the outsider, the AI was right in " + str(100.0 * (len(Out_pred[right_out]) / float(len(Out_pred)))) + "% of cases")
+
 
 
